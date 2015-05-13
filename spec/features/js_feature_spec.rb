d41d2fd30894ae1805f5cf3c1433593ed96fc284
joker1007 <kakyoin.hierophant@gmail.com>
2015-05-13 18:08:31 +0900

    Fix function names

--- spec/features/js_feature_spec.rb
+++ spec/features/js_feature_spec.rb
@@ -4,22 +4,25 @@ RSpec.feature "RailsHelper", js: true do
   scenario "is compiled properly" do
     visit "/users"
 
-    expect(page.evaluate_script "RailsHelper.named_route('users')")
+    expect(page.evaluate_script "RailsHelper.pathFor('users')")
       .to eq "/users"
 
-    expect(page.evaluate_script "RailsHelper.named_route('users', {'format': 'json'})")
+    expect(page.evaluate_script "RailsHelper.pathFor('users', {'format': 'json'})")
       .to eq "/users.json"
 
-    expect(page.evaluate_script "RailsHelper.route_names()")
+    expect(page.evaluate_script "RailsHelper.urlFor('users', {'format': 'json'})")
+      .to match(%r(http://#{Regexp.escape(Capybara.server_host)}:\d+/users.json))
+
+    expect(page.evaluate_script "RailsHelper.routeNames()")
       .to match_array ["users", "new_user"]
 
-    expect(page.evaluate_script "RailsHelper.asset_path('scaffold.css')")
+    expect(page.evaluate_script "RailsHelper.assetPath('scaffold.css')")
       .to eq "/assets/scaffold.css"
 
-    expect(page.evaluate_script "RailsHelper.image_path('dummy.png')")
+    expect(page.evaluate_script "RailsHelper.imagePath('dummy.png')")
       .to eq "/assets/dummy.png"
 
-    expect(page.evaluate_script "RailsHelper.image_tag('dummy.png', {'width': '120'})")
+    expect(page.evaluate_script "RailsHelper.imageTag('dummy.png', {'width': '120'})")
       .to eq "<img src=\"/assets/dummy.png\" width=\"120\">"
   end
 end
